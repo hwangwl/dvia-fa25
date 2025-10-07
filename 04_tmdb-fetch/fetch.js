eccc3254b6606fa3b629137e6c46be0e2ef4cf4b
hwangwl <hwangw@newschool.edu>
2025-10-07 10:42:44 -0400

    Update fetch.js

--- 04_tmdb-fetch/fetch.js
+++ 04_tmdb-fetch/fetch.js
@@ -4,7 +4,7 @@
 
 
 // Use Bearer token in Authorization header, not as api_key param
-const token = "";
+const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzI4NzhkZGI1NWNlYzlmZWU2YmQ3NzUzMzYyN2JkOSIsIm5iZiI6MTc1OTg0NTMzOS45OTQsInN1YiI6IjY4ZTUxYmRiMGMyNWQzMzc4NWMyMzAwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JFSuYHfLWUx-JmDOQAMJVOIvdJ3y0LsDGevxzRlBob0";
 const query = "Jack Reacher";
 const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`;
 const options = {
